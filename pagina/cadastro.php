f8ff8a281cb81b5a5a09beec6c7de3b7ee0a9faf
unknown <carlosneto@outlook.com>
2018-05-23 17:22:17 -0300

    Formulário de cadastro

--- pagina/cadastro.php
+++ pagina/cadastro.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="pt-br">
     <head>
         <!-- Required meta tags -->
@@ -9,6 +9,17 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
         <title>Cadastro!</title>
+        <script type="text/javascript">
+          function validar()
+          {
+            if(document.getElementById("senha").value!==document.getElementById("senhaConf").value)
+            {
+              alert("Senha inválida!");
+              return false;
+            }
+            return true;
+          }
+        </script>
     </head>
 
     <body>
@@ -17,7 +28,19 @@
         <main class="container" role="main">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6 jumbotron">
+                      <h3>Preencha os campos</h3>
+                      <form action="cadastroGet.php" onsubmit="return validar();" method="post">
+                        Nome <br/>
+                        <input type="text" name="nome" required/><br/>
+                        E-mail <br/>
+                        <input type="email" name="email" required/><br/>
+                        Senha <br/>
+                        <input type="password" name="senha" id="senha" required/><br/>
+                        Confirmar senha <br/>
+                        <input type="password" name="senhaConf" id="senhaConf" required/><br/><br/>
+                        <input type="submit" value="Enviar"/> <input type="reset" value="Limpar"/>
+                      </form>
                     </div>
                     <div class="col-md-6">
                         <div class="row">
@@ -27,7 +50,7 @@
                         </div>
                         <div class="row">
                             <div class="col-md-12">
-                                <?php include('estrutura/mapa.php')?>
+                                <?php include('mapa.php')?>
                             </div>
                         </div>
                     </div>
