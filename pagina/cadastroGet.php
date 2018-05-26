c77d65141f6d821e0bced37f79a6261ee98b6753
unknown <carlosneto@outlook.com>
2018-05-26 12:28:55 -0300

    Inserção de usuário (cadastroGet.php)

---
+++ pagina/cadastroGet.php
@@ -0,0 +1,16 @@
+﻿<?php
+include_once("conexao/conexao.php");
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+//echo $senha;
+if ($con=abreConexao()){
+$ps=mysqli_prepare($con,"INSERT INTO USUARIO (NOME,EMAIL,SENHA) VALUES(?,?,?)");
+mysqli_stmt_bind_param($ps,"sss",$nome,$email,$senha);
+mysqli_stmt_execute($ps);
+echo "Cadastro finalizado com sucesso (será?)!";
+}
+else {
+  echo "Azedou!";
+}
+?>
